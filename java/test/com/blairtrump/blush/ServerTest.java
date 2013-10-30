efaf7e9a5c998b47998febd53df74e7fc8104502
Blair Trump <blair@blairtrump.com>
2013-10-29 20:47:50 -0400

    Added unit tests for Packet and Server classes

--- java/test/com/blairtrump/blush/ServerTest.java
+++ java/test/com/blairtrump/blush/ServerTest.java
@@ -7,32 +7,7 @@ import org.junit.Test;
 public class ServerTest {
 
 	@Test
-	public void testServer() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMethodSystem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMethodApplication() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testInitialize() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testListen() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMain() {
+	public void test() {
 		fail("Not yet implemented");
 	}
 
